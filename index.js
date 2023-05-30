82b9475e47467dcfa6dc97ffa98a99c1250f9a50
Nitin Babu <68281837+the-madman-28@users.noreply.github.com>
2023-05-30 06:46:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ image2.setAttribute("src", randomImageSource2);
 
 if(randomNumber1 > randomNumber2)
 {
-    document.querySelector("h1").innerHTML="Player 1 Wins !!!";
+    document.querySelector("h1").innerHTML="Player 1 Won !!!";
 }
 
 else if(randomNumber2 > randomNumber1)
 {
-    document.querySelector("h1").innerHTML="Player 2 Wins !!!";
+    document.querySelector("h1").innerHTML="Player 2 Won !!!";
 }
 
 else
